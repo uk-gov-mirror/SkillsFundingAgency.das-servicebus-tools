ee53e6a2dc3fb9c279e1f0fc0207d08b19942992
skumardfe <Santhosh.KUMAR@education.gov.uk>
2026-09-04 12:09:22 +0100

    Add approved learning updated function

---
+++ src/SFA.DAS.ServiceBus.Tools.Functions/Functions/ApprovedLearningUpdatedFunction.cs
@@ -0,0 +1,21 @@
+﻿using SFA.DAS.ServiceBus.Tools.Functions.Services;
+
+namespace SFA.DAS.ServiceBus.Tools.Functions.Functions;
+
+public class ApprovedLearningUpdatedFunction(IMessageProcessor messageProcessor)
+{
+    [Function("ApprovedLearningUpdated")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post", "ApprovedLearningUpdated")] HttpRequestData req, FunctionContext functionContext)
+    {
+        try
+        {
+            await messageProcessor.PublishEvent<ApprovedLearningUpdatedEvent>(req.Body, functionContext);
+        }
+        catch
+        {
+            return req.CreateResponse(HttpStatusCode.BadRequest);
+        }
+
+        return req.CreateResponse(HttpStatusCode.OK);
+    }
+}
